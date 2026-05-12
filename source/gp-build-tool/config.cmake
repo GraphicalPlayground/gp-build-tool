ff81bd3eb34b41bc968312a3dea8fae10dfb806f
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-12 13:58:14 +0200

    add: update configuration options for CI environment and target properties dumping

--- source/gp-build-tool/config.cmake
+++ source/gp-build-tool/config.cmake
@@ -9,9 +9,13 @@ set(GPBT_CURRENT_VERSION "0.4.0")
 option(GPBT_TESTS_ENABLED "Set to true to run build tool tests" FALSE)
 option(GPBT_TESTS_FILTER_SECTION "If set, only run test sections that contain this string in their name" "")
 
+# Specific options
+option(GPBT_IS_RUNNED_IN_CI "Set to true if the build tool is being run in a CI environment" FALSE)
+
 # Logging
 option(GPBT_LOG_PREFIX_ENABLED "Set to true to enable log prefix" FALSE)
 option(GPBT_TREAT_WARNINGS_AS_FATAL "Set to true to treat warnings as fatal errors" FALSE)
 option(GPBT_TREAT_ERRORS_AS_FATAL "Set to true to treat errors as fatal" FALSE)
 option(GPBT_LOG_VERBOSE_ENABLED "Set to true to enable verbose logging" FALSE)
 option(GPBT_LOG_BANNER_ENABLED "Set to true to enable log banner" TRUE)
+option(GPBT_DUMP_TARGETS_PROPERTIES "Set to true to dump target properties after configuration" FALSE)
