257953fceaabcfd1aead7279e8f305b42903e065
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-12 11:25:48 +0200

    add: create all.cmake test suite file with initial setup

---
+++ source/gp-build-tool/tests/all.cmake
@@ -0,0 +1,12 @@
+# Copyright (c) - Graphical Playground. All rights reserved.
+# For more information, see https://graphical-playground/legal
+# mailto:support AT graphical-playground DOT com
+
+include(gp-build-tool/tests/asserts)
+
+gpbt_setupTestSuite()
+
+include(gp-build-tool/tests/utilities/strings)
+include(gp-build-tool/tests/utilities/properties)
+
+gpbt_dumpTestStats()
