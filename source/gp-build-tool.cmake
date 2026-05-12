47e080bb4b9d96245664c6438d88065a3b267863
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-12 11:26:10 +0200

    add: include configuration and test suite handling in gp-build-tool.cmake

--- source/gp-build-tool.cmake
+++ source/gp-build-tool.cmake
@@ -1,3 +1,10 @@
 # Copyright (c) - Graphical Playground. All rights reserved.
 # For more information, see https://graphical-playground/legal
 # mailto:support AT graphical-playground DOT com
+
+include(gp-build-tool/config)
+
+if(GPBT_TESTS_ENABLED)
+  include(gp-build-tool/tests/all)
+  return()
+endif()
