039df3d994b1acbcdae237049626363635bebdf4
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-12 11:39:19 +0200

    add: include logger and log banner functionality in gp-build-tool.cmake

--- source/gp-build-tool.cmake
+++ source/gp-build-tool.cmake
@@ -3,6 +3,11 @@
 # mailto:support AT graphical-playground DOT com
 
 include(gp-build-tool/config)
+include(gp-build-tool/utilities/logger)
+
+if(GPBT_LOG_BANNER_ENABLED)
+  gpbt_logBanner()
+endif()
 
 if(GPBT_TESTS_ENABLED)
   include(gp-build-tool/tests/all)
