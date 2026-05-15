589c4788f82bbdd2e5356bad40b67e44360ea366
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-15 15:10:40 +0200

    feat: add main function to output "Hello, World!" message

--- examples/engine/source/launch/editor/private/Private.cpp
+++ examples/engine/source/launch/editor/private/Private.cpp
@@ -1,3 +1,11 @@
 // Copyright (c) - Graphical Playground. All rights reserved.
 // For more information, see https://graphical-playground/legal
 // mailto:support AT graphical-playground DOT com
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
